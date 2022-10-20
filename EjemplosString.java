ac215717aded19b92d935ddd15b2c24fbb924191
RicardoMorenoCantea <115982761+RicardoMorenoCantea@users.noreply.github.com>
2022-10-20 14:07:45 +0200

    Add files via upload

---
+++ EjemplosString.java
@@ -0,0 +1,12 @@
+public class EjemplosString {
+    public static void main(String[] args) {
+        //Declaración de variables
+        String nm = "Ricardo";
+        String ap = "Moreno";
+        float edad = 18;
+        //Impresión con formato
+        System.out.printf("Me llamo %s %s tengo %.2f años",nm,ap,edad);
+        System.out.println("");
+        System.out.println("Esto \\ es una barra invertida");
+    }
+}
